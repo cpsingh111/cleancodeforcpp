6a312c4d45ca0f220fbea6f01f0a208d38a82bbf
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-20 15:28:46 +0530

    Update paramchecker.cpp
    function splitting

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,6 +1,19 @@
 
+bool bpmokfun(float bpm){
+  return(bpm<70|| bpm>150);
+}
+
+bool spo2fun(float spo2){
+  return(spo2<80);
+}
+
+bool respRatefun(float respRate){
+  return(respRate<30|| respRate>60); 
+}
+
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
+/*  if(bpm < 70 || bpm > 150) {
     return false;
   } 
   if(spo2 < 80) {
@@ -8,6 +21,6 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
   } 
   if(respRate < 30 || respRate > 60) {
     return false;
-  }
-  return true;
+  }*/
+  return (bpmokfun(bpm)||spo2fun(spo2)||respRatefun(respRate));
 }
