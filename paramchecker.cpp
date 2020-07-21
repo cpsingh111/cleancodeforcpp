b3febc549eec4c63312701aa22761fd592f6fbc1
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-21 16:30:27 +0530

    Updated logical condition check

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,8 +1,8 @@
 bool iswithinrange(float arg, float min, float max){
-   return(arg<min||arg>max); 
+   return(arg>min&&arg<max); 
 }
 
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (iswithinrange(bpm,70,150)||iswithinrange(spo2,80,100)||iswithinrange(respRate,30,60));
+  return (iswithinrange(bpm,70,150)&&iswithinrange(spo2,80,100)&&iswithinrange(respRate,30,60));
 }
