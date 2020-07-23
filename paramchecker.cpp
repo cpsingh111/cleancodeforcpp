d467b636c2ca573f02010a847c59e3ed1186e785
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-23 14:17:24 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -7,11 +7,11 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
   return (iswithinrange(bpm,70,150)&&iswithinrange(spo2,80,100)&&iswithinrange(respRate,30,60));
 }*/
 
-
+#include "paramchecker.h"
 bool vitalsAreOk(float *arr, int n) {
   
-   static bool Vitalsok;
-   for(int i=0; i<n, i++)
+   static bool vitalsok;
+   for(int i=0; i<n; i++)
    {
        vitalsok|= iswithinrange(arr[i]);
    }
