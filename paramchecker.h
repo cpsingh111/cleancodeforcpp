b6f72bf0766b23f9001867e3b69a7d092137fea0
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-23 14:09:20 +0530

    Updated to solve OCP. Introduced class for Vitals

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,40 @@
+bool vitalsAreOk(float *arr, int n);
 
-bool vitalsAreOk(float bpm, float spo2, float respRate);
+class Vitals
+{
+ float upperbound=0;
+ float lowerbound=0;
+
+ public: virtual bool iswithinrange(float vital){
+ 
+ return false;
+ }
+ 
+}
+
+class bpm:public Vitals
+{
+ float upperbound= 150;
+ float lowerbound=70;
+ 
+ public: virtual bool iswithinrange(float bpm){
+  return(bpm>upperbound&&bpm<lowebound);
+ }
+
+class spo2:public Vitals
+{
+ float upperbound= 101;
+ float lowerbound=80;
+ 
+ public: virtual bool iswithinrange(float spo2){
+  return(spo2>upperbound&&spo2<lowerbound);
+ }
+ 
+ class respRate:public Vitals
+{
+ float upperbound= 60;
+ float lowerbound=30;
+ 
+ public: virtual bool iswithinrange(float respRate){
+  return(respRate>upperbound&&respRate<lowerbound);
+ }
