b7d5dd0f8f2ccb5b014ceb6f9968740770739c32
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-23 14:09:48 +0530

    Update for OCP.

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,8 +1,20 @@
-bool iswithinrange(float arg, float min, float max){
+/*bool iswithinrange(float arg, float min, float max){
    return(arg>min&&arg<max); 
 }
 
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   return (iswithinrange(bpm,70,150)&&iswithinrange(spo2,80,100)&&iswithinrange(respRate,30,60));
+}*/
+
+
+bool vitalsAreOk(float *arr, int n) {
+  
+   static bool Vitalsok;
+   for(int i==; i<n, i++)
+   {
+       vitalsok|= iswithinrange(arr[i]);
+   }
+   
+   return (vitalsok);
 }
