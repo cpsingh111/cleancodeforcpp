c82d36b7ebeee938c3536cc72f48cfc6857ced44
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-07-23 14:35:21 +0530

    Updated testcases to pass arr

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,21 +1,26 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
- 
+
+float bpm[3]={60,90,50}; 
+float spo2[3]={100,40,50};
+float Resprate[3]={100,90,70};
+float vital[3]={100,90,50};
+
 TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(false, vitalsAreOk(60, 90, 50));
+    ASSERT_EQ(false, vitalsAreOk(bpm, 3));
 }
  
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+    ASSERT_EQ(false, vitalsAreOk(spo2, 3));
 }
  
  
 TEST(VitalsTest, RespRate) {
-    ASSERT_EQ(false, vitalsAreOk(100, 90, 70));
+    ASSERT_EQ(false, vitalsAreOk(Resprate,3));
 }
 
 TEST(VitalsTest, Pass) {
-    ASSERT_EQ(true, vitalsAreOk(100, 90, 50));
+    ASSERT_EQ(true, vitalsAreOk(vital, 3));
 }
  
 int main(int argc, char **argv) {
